25f3f0744f7cfa5663ea8e6d76e69506e1b85047
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-10-03 18:22:44 -0400

    chore: configure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,43 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+    'prettier',
+  ],
   env: {
     browser: true,
     es2022: true,
+    node: true,
   },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
   },
-  rules: {},
 };
