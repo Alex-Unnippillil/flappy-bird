be907650bfd82b0ab75ed2f89b3c30ff4adaa5f8
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-10-03 21:24:38 -0400

    Add feature-flagged bird asset loader

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,2 +1,4 @@
 export { bus } from './event-bus';
 export type { GameEventName } from './event-bus';
+export { loadBird } from './assets';
+export type { LoadedBird, LoadBirdOptions } from './assets';
